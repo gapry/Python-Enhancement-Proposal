e3376c06ff84afd6cf9c5209ce1e82d1d8a88b5a
gapry <gapry@protonmail.com>
2020-01-14 09:11:13 +0800

    [PEP572] utilize Keyword-Only Arguments to simplify

--- PEP572/main.py
+++ PEP572/main.py
@@ -5,13 +5,11 @@ def get_pair(threshold, upper, best_value, alpha, beta) -> (int, int):
     if (((alpha := best_value << 2) if (threshold == target) else (beta := best_value >> 1) >= beta)): break
   return (alpha, beta)
 
-def show_pair(alpha, beta):
-  print("alpha = %d" % alpha)
-  print("beta  = %d" % beta)
+def show_pair(*pair: (int, int)):
+  print(("(alpha, beta) = ({0}, {1})").format(str(pair[0]), str(pair[1])))
 
 if __name__ == '__main__':
   alpha = random.randint(1, 2 << 1)
   beta  = random.randint(alpha << 1, alpha << 2)
   show_pair(alpha, beta)
-  alpha, beta = get_pair(random.randint(1, 10), 16, random.randint(2, 8), alpha, beta)
-  show_pair(alpha, beta)
+  show_pair(*get_pair(random.randint(1, 10), 16, random.randint(2, 8), alpha, beta))
