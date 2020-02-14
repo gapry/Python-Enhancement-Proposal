2f5dfa52ea925ff69761bd446f56ddcb337dc677
gapry <gapry@protonmail.com>
2020-02-14 18:06:52 +0800

    [PEP318] modify sum_list to even_list

--- PEP318/main.py
+++ PEP318/main.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 
-def sum_list(f):
+def even_list(f):
+  predicate = lambda x: x == x >> 1 << 1
   def wrap(*args, **kwargs):
     print(f'*args      = {args}')
     print(f'**kwargs   = {kwargs}')
     print(f'f.__name__ = {f.__name__}')
-    return sum(f(*args, **kwargs))
+    return list(filter(predicate, f(*args, **kwargs)))
   return wrap
 
-@sum_list
+@even_list
 def make_list(*args, **kwargs):
-  predicate = lambda x: x >> 1 << 1
-  xs = [elem for elem in args if elem == predicate(elem)] 
-  ys = [value for value in kwargs.values() if value != predicate(value)]
-  return xs + ys
+  return [elem for elem in args] + [value for value in kwargs.values()]
 
 def main():
-  print(f'sum_list(make_list) = {make_list(1, 2, 3, x = 4, y = 5, z = 6)}')
+  print(f'even_list(make_list) = {make_list(1, 2, 3, x = 4, y = 5, z = 6)}')
 
 if __name__ == '__main__':
   main()
