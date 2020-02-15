1c04c6d751865a338d2441f006d8eadb5b8c2e6a
gapry <gapry@protonmail.com>
2020-02-15 10:11:17 +0800

    [PEP318] add docstrings for each function

--- PEP318/main.py
+++ PEP318/main.py
@@ -2,14 +2,18 @@
 
 class log():
   def __call__(self, f):
+    "take the class wrap as a return"
     def wrap(*args, **kwargs):
+      "output the runtime information"
       print(f'[log] ${f}')
       return f(*args, **kwargs)
     return wrap
 
 def even_list(f):
+  "take the class wrap as a return"
   predicate = lambda x: x == x >> 1 << 1
   def wrap(*args, **kwargs):
+    "only accept even numbers to generate a new list"
     print(f'*args      = {args}')
     print(f'**kwargs   = {kwargs}')
     print(f'f.__name__ = {f.__name__}')
@@ -19,10 +23,12 @@ def even_list(f):
 @log()
 @even_list
 def make_list(*args, **kwargs):
+  "utilize the function parameters to generate a new list"
   print(f'[even_list closure] {even_list(make_list).__closure__}')
   return list(args) + list(kwargs.values())
 
 def main():
+  "execute the test case"
   print(f'even_list(make_list) = {make_list(1, 2, 3, x = 4, y = 5, z = 6)}')
 
 if __name__ == '__main__':
